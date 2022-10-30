b7ae764538100d78621b5ba081c801ce0427505e
SpBarber <santipbr@gmail.com>
2022-10-30 16:33:31 +0100

    Commit control
    Contenedores estructurales terminados.
    
    Modificando botones

---
+++ Trivia/Prototypes/UIButton+Text.swift
@@ -0,0 +1,50 @@
+//
+//  UIButton+Text.swift
+//  Trivia
+//
+//  Created by Santiago Pérez Barber on 30/10/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+  func cmButtonResponse(
+    title: String,
+    titleFontSize: CGFloat = 28,
+    titleAlignment: UIButton.Configuration.TitleAlignment = .leading,
+    titlePadding: CGFloat = 4,
+    imageName: String = "",
+    imagePadding: CGFloat = 6,
+    imagePlacement:  NSDirectionalRectEdge = .leading,
+    cornerRadius: CGFloat = 50
+    //backgroundColor: UIColor = .,
+    //foregroundColor: UIColor = .white
+  ) {
+    print(#function)
+    var config = UIButton.Configuration.gray()
+    // Text
+    config.title = title
+    var attText = AttributedString(title)
+    attText.font = UIFont.systemFont(ofSize: titleFontSize)
+    config.attributedTitle = attText
+    config.titleAlignment = .leading
+    config.titlePadding = titlePadding
+    
+    // Image
+    config.image = UIImage(systemName: imageName)
+    config.imagePadding = imagePadding
+    config.imagePlacement = imagePlacement
+    
+    //config.cornerStyle = .large
+    // Outline
+    config.background.cornerRadius = cornerRadius
+    config.background.strokeWidth = 20
+    config.background.strokeColor = .white
+//    config.baseBackgroundColor = backgroundColor
+//    config.baseForegroundColor = foregroundColor
+    
+    // Inject config
+    self.configuration = config
+  }
+}
--- Trivia/ViewController.swift
+++ Trivia/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var trueButton: UIButton!
+  @IBOutlet weak var falseButton: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    configButtons()
     // Do any additional setup after loading the view.
+  }
+  
+  private func configButtons() {
+    trueButton.cmButtonResponse(title: "True")
+    falseButton.cmButtonResponse(title: "False")
   }
 
 
